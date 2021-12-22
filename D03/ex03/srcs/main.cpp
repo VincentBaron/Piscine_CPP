0e19855c12e9d7e4fd598469854931d8271f0052
VincentBaron <vincentbaron1996@gmail.com>
2021-12-22 17:27:55 +0100

    finished D03

--- D03/ex03/srcs/main.cpp
+++ D03/ex03/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vbaron <vbaron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/20 19:37:35 by vbaron            #+#    #+#             */
-/*   Updated: 2021/12/22 16:58:10 by vbaron           ###   ########.fr       */
+/*   Updated: 2021/12/22 17:27:30 by vbaron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ int main(void)
     warrior1.takeDamage(20);
     warrior1.beRepaired(50);
     warrior1.whoAmI();
+    warrior1.highFiveGuys();
 
     return 0;
 }
